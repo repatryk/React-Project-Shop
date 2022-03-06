fa04cd71f85a08dad04e8a1d0ef3912d24355f7d
Patryk <repatryk@gmail.com>
2022-03-06 17:58:42 +0100

    Shop

---
+++ src/data.js
@@ -0,0 +1,32 @@
+import img1 from './img/img1.jpg'
+import img2 from './img/img2.jpg'
+import img3 from './img/img3.jpg'
+
+const data = {
+    productData: [
+        {
+            id: 1,
+            img: img1,
+            title: "MacBook Pro",
+            desc: 'Najnowszej generacji Macbook Pro. Gwarantuje pełną satysfakcję z użytkowania. Wyposażony w najnowszej generacji system. Umożliwia w pełni funkcjonalne korzystanie z internetu.',
+            price: 3560
+        },
+        {
+            id: 2,
+            img: img2,
+            title: "Samsung S22",
+            desc: 'Najnowszej generacji telefon został wyposażony w funkcje sterujące głosem. Świetnie sprawdza się w każdych okolicznościach. Zakup w pełni opłacalny. Kup już dziś!',
+            price: 4200
+
+        },
+        {
+            id: 3,
+            img: img3,
+            title: "Aparat X9000",
+            desc: 'Fantastyczny aparat z najnowszymi funkcjami. Świetnie sprawdza się w każdych warunkach. Ostrość i głębia ukazują prawdziwą moc tego wyjątkowego sprzętu.',
+            price: 8000
+        }
+    ],
+};
+
+export default data;
